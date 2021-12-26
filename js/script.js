8bdb91a76df18e3b9a967d7034102bc272e310d5
Artem <example@gmail.com>
2021-12-26 23:39:49 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(incr++);
+console.log(decr--);
+console.log(incr++);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 === '8');
+
+&&
+||
+
+const isChecked = true,
+      isClose = false;
+      
+console.log(isChecked || isClose);
